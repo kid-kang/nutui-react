345bdb6e4bf04e391b24c9ab20359b01959dd048
Alex.hxy <1872591453@qq.com>
2024-11-11 22:39:47 +0800

    chore: update

--- src/packages/progress/demos/taro/demo4.tsx
+++ src/packages/progress/demos/taro/demo4.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Progress, Image, Cell } from '@nutui/nutui-react-taro'
-import pxTransform from '@/utils/px-transform'
 
 const Demo4 = () => {
   return (
     <Cell>
       <Progress percent={60} showText>
         <Image
-          width={pxTransform(20)}
-          height={pxTransform(20)}
+          width={20}
+          height={20}
           src="https://img12.360buyimg.com/imagetools/jfs/t1/229362/18/22746/11607/669f8bfeF5fcbeaab/d4162bbf29bb1b00.png"
         />
       </Progress>
